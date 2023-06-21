9e1bb1eec239bf3339bd38c1c6a1d8944f754a9c
CarlosPisco <edupiscote.cept@gmail.com>
2023-06-20 19:16:10 -0500

    first commit

---
+++ src/main/java/com/example/lab9/Controller/MasterController.java
@@ -0,0 +1,21 @@
+package com.example.lab9.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/inicio")
+public class MasterController {
+
+    @GetMapping("")
+    public String principal(){
+
+
+        return "index";
+    }
+
+
+
+}
